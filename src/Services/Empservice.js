6cceae8f9f37f904ba557cd5f017e72322c11bf0
Akhila-Reddy-stack <60685579+Akhila-Reddy-stack@users.noreply.github.com>
2021-06-05 18:05:10 -0800

    Add files via upload

--- src/Services/Empservice.js
+++ src/Services/Empservice.js
@@ -1,1 +1,9 @@
+import http from './httpService';
+import { apiUrl } from "../config.json";
+
+export function Empregistration(data) {
+    const apiEndPoint = `${apiUrl}/empregistration`;
+    console.log(apiEndPoint)
+    return http.post(`${apiEndPoint}`, data);
+}
 
