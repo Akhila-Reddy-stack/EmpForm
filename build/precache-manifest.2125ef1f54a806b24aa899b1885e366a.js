d91a9a10fe6c5fb6cc90ac0febcdaacab957bb56
Akhila-Reddy-crm <areddy@crmwebx.com>
2022-10-26 09:29:43 +0530

    Added bild folder and git ignore

---
+++ build/precache-manifest.2125ef1f54a806b24aa899b1885e366a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e3b8a6a3c1419904b0f95821f681166",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a8060bc4f6498e29e09",
+    "url": "/static/css/main.968d19fa.chunk.css"
+  },
+  {
+    "revision": "ffa20a3867910f37dbac",
+    "url": "/static/js/2.99d75f57.chunk.js"
+  },
+  {
+    "revision": "e3cddcc14d0b349b4e6eccba623e55ae",
+    "url": "/static/js/2.99d75f57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a8060bc4f6498e29e09",
+    "url": "/static/js/main.8f0e0348.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  }
+]);
